4a13456dfcd15ba2d14455ff01e5774c243b67ac
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-04-05 12:18:08 -0300

    Filtros Avançados em APIs com Specification Avançado para Relacionamento e Pageable

--- EAD/course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ EAD/course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -3,6 +3,7 @@ package com.ead.course.repositories;
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID>, JpaSpecificationExecutor<ModuleModel> {
 
     @EntityGraph(attributePaths = {"course"})
     ModuleModel findByTitle(String title);
