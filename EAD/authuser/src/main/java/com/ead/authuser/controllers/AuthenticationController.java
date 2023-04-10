b3383e7badc4b36638a8a223c6aea3397fd7fcc8
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-04-10 19:57:21 -0300

    Implementando Logging nos Microservices com Log4J2

--- EAD/authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ EAD/authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -6,8 +6,7 @@ import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,12 +17,11 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
-
-    private final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     @Autowired
     private UserService userService;
@@ -33,10 +31,13 @@ public class AuthenticationController {
         @RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
         @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto
     ) {
+        log.debug("POST registerUser userDto received {} ", userDto.toString());
         if (userService.existsByUsername(userDto.getUsername())) {
+            log.warn("Username {} is Already Taken", userDto.getUsername());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
         }
         if (userService.existsByEmail(userDto.getEmail())) {
+            log.warn("Email {} is Already Taken", userDto.getEmail());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
         }
 
@@ -49,17 +50,19 @@ public class AuthenticationController {
 
         userService.save(userModel);
 
+        log.debug("POST registerUser userModel saved {} ", userModel.toString());
+        log.info("User saved successfully userId {} ", userModel.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
 
     @GetMapping("/")
     public String index() {
         // mvn spring-boot:run -Dspring-boot.run.arguments=--logging.level.com.ead=TRACE
-        logger.trace("TRACE");
-        logger.debug("DEBUG");
-        logger.info("INFO");
-        logger.warn("WARN");
-        logger.error("ERROR");
+        log.trace("TRACE");
+        log.debug("DEBUG");
+        log.info("INFO");
+        log.warn("WARN");
+        log.error("ERROR");
         return "Logging Spring Boot...";
     }
 
--- EAD/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ EAD/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +24,7 @@ import java.util.UUID;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
@@ -59,11 +61,15 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
+        log.debug("DELETE deleteUser userId received {} ", userId);
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         }
         userService.delete(userModelOptional.get());
+
+        log.debug("DELETE deleteUser userId deleted {} ", userId);
+        log.info("User deleted successfully userId {} ", userId);
         return ResponseEntity.status(HttpStatus.OK).body("User deleted with success!");
     }
 
@@ -73,6 +79,7 @@ public class UserController {
         @RequestBody @Validated(UserDto.UserView.UserPut.class)
         @JsonView(UserDto.UserView.UserPut.class) UserDto userDto
     ) {
+        log.debug("PUT updateUser userDto received {} ", userDto.toString());
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
@@ -86,6 +93,8 @@ public class UserController {
 
         userService.save(userModel);
 
+        log.debug("PUT updateUser userModel saved {} ", userModel.toString());
+        log.info("User updated successfully userId {} ", userModel.getUserId());
         return ResponseEntity.status(HttpStatus.OK).body(userModel);
     }
 
@@ -102,6 +111,7 @@ public class UserController {
         }
 
         if (!userModelOptional.get().getPassword().equals(userDto.getOldPassword())) {
+            log.warn("Mismatched old password userId {} ", userDto.getUserId());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Mismatched old password!");
         }
 
