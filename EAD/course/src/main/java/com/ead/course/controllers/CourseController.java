d1f8585cdd000bfc797e9100e5d76be07a43e708
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-05-17 19:36:50 -0300

    Validação Customizada - Custom Validation.

--- EAD/course/src/main/java/com/ead/course/controllers/CourseController.java
+++ EAD/course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -4,6 +4,8 @@ import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.specifications.SpecificationTemplate;
+import com.ead.course.validation.CourseValidator;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,17 +14,18 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static java.util.Objects.nonNull;
 
+@Log4j2
 @RestController
 @RequestMapping("/courses")
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -31,13 +34,30 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
+    @Autowired
+    private CourseValidator courseValidator;
+
     @PostMapping
-    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto) {
+    public ResponseEntity<Object> saveCourse(@RequestBody CourseDto courseDto, Errors errors) {
+        log.debug("POST saveCourse courseDto received {} ", courseDto.toString());
+
+        courseValidator.validate(courseDto, errors);
+
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
+        }
+
         var courseModel = new CourseModel();
         BeanUtils.copyProperties(courseDto, courseModel);
         courseModel.setCreationDate(LocalDateTime.now(ZoneId.of(("UTC"))));
         courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of(("UTC"))));
-        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel));
+
+        courseService.save(courseModel);
+
+        log.debug("POST saveCourse courseModel saved {} ", courseModel.toString());
+        log.info("Course saved successfully courdeId {} ", courseModel.getCourseId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseModel);
     }
 
     @DeleteMapping("/{courseId}")
