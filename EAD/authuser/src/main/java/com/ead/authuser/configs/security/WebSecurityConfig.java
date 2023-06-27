e079c6812d0a5b80ceabf4d3e5004de603f36265
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-06-27 15:39:46 -0300

    Implementar UserDetails e UserDetailsService para Basic Auth com Database

--- EAD/authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ EAD/authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.ead.authuser.configs.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +17,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthenticationEntryPointImpl authenticationEntryPoint;
+
     private static final String[] AUTH_WHITELIST = {
         "/auth/**"
     };
@@ -23,9 +31,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .httpBasic()
+            .authenticationEntryPoint(authenticationEntryPoint)
             .and()
             .authorizeRequests()
             .antMatchers(AUTH_WHITELIST).permitAll()
+            .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and()
             .csrf().disable();
@@ -33,11 +43,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("admin")
-            .password(passwordEncoder().encode("123456"))
-            .roles("ADMIN");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .httpBasic()
+//            .and()
+//            .authorizeRequests()
+//            .antMatchers(AUTH_WHITELIST).permitAll()
+//            .anyRequest().authenticated()
+//            .and()
+//            .csrf().disable();
+//    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//            .withUser("admin")
+//            .password(passwordEncoder().encode("123456"))
+//            .roles("ADMIN");
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
