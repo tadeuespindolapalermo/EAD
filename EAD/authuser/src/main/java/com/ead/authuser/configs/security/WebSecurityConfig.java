c99d6b47d8f94fab24ebdfd89805c9b1b0b3a737
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-06-30 15:12:11 -0300

    Implementar Validation Tokens e Authorization com JWT

--- EAD/authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ EAD/authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -10,8 +10,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,18 +30,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         "/auth/**"
     };
 
+    @Bean
+    public AuthenticationJwtFilter authenticationJwtFilter() {
+        return new AuthenticationJwtFilter();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .httpBasic()
+            .exceptionHandling()
             .authenticationEntryPoint(authenticationEntryPoint)
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests()
             .antMatchers(AUTH_WHITELIST).permitAll()
-            .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and()
-            .csrf().disable();
+            .csrf()
+            .disable();
+
+        http.addFilterBefore(authenticationJwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
