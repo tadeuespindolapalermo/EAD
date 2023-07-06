f9febdab2c1163778d732f21eb9fdfab14c5cbd3
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-07-06 13:49:02 -0300

    Access Token na Comunicação Síncrona entre Microservices

--- EAD/authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ EAD/authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -12,6 +12,8 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -38,24 +40,25 @@ public class CourseClient {
     //@Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     //@CircuitBreaker(name = "circuitbreakerInstance", fallbackMethod = "circuitbreakerfallback")
     @CircuitBreaker(name = "circuitbreakerInstance")
-    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable, String token){
         List<CourseDto> searchResult;
-        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+        ResponseEntity<ResponsePageDto<CourseDto>> result;
         String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token);
+        HttpEntity<String> requestEntity = new HttpEntity<>("parameters", headers);
 
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
 
         System.out.println("---Start Request to Course Microservice---");
-        try{
-            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<>() {};
-            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
-            searchResult = result.getBody().getContent();
-            log.debug("Response Number of Elements: {} ", searchResult.size());
-        } catch (HttpStatusCodeException e){
-            log.error("Error request /courses {} ", e);
-        }
 
+        ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<>() {};
+        result = restTemplate.exchange(url, HttpMethod.GET, requestEntity, responseType);
+        searchResult = result.getBody().getContent();
+
+        log.debug("Response Number of Elements: {} ", searchResult.size());
         log.info("Ending request /courses userId {} ", userId);
 
         return result.getBody();
