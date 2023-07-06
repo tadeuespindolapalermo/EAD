79881ce2a9a5b4254dd27a2249ed2109b473c902
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-07-06 14:55:42 -0300

    Authorization Roles para Instructor

--- EAD/authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ EAD/authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,13 +1,17 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,6 +28,10 @@ public class InstructorController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
         Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
@@ -32,9 +40,13 @@ public class InstructorController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
         }
 
+        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+            .orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
+
         var userModel = userModelOptional.get();
         userModel.setUserType(UserType.INSTRUCTOR);
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.getRoles().add(roleModel);
 
         userService.updateUser(userModel);
 
