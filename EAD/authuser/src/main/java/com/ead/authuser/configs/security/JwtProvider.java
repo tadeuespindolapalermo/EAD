60dacfebfd292d77347313f4373f98c1c6cd6519
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2023-12-28 16:35:52 -0300

    fix

--- EAD/authuser/src/main/java/com/ead/authuser/configs/security/JwtProvider.java
+++ EAD/authuser/src/main/java/com/ead/authuser/configs/security/JwtProvider.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import jakarta.crypto.SecretKey;
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.stream.Collectors;
